c892a8d2d55eb0f6c3107dc0e84d831abe1f3180
Nicolas TOURRETTE <nicotou1998@gmail.com>
2020-03-01 16:05:59 +0100

    Create account form finished (without validator)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,7 +6,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+use App\Form\UserType;
+
+use App\Entity\User;
 
 
 class SecurityController extends AbstractController
@@ -21,6 +27,34 @@ class SecurityController extends AbstractController
 		return $this->render('Security/login.html.twig', array(
 			'last_username' => $authenticationUtils->getLastUsername(),
 			'error'         => $authenticationUtils->getLastAuthenticationError(),
+		));
+	}
+
+	/**
+	 * @Route("/register", name="register")
+	 */
+	public function register(Request $request){
+		if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+			throw new AccessDeniedException('Vous êtes déjà un utilisateur authentifié.');
+		}
+
+		$em = $this->getDoctrine()->getManager();
+		$user = new User();
+
+		$form = $this->get('form.factory')->create(UserType::class, $user);
+
+		if ($request->isMethod('POST')) {
+			$form->handleRequest($request);
+			if ($form->isValid()) {
+				$em->persist($advert);
+				$em->flush();
+
+				return $this->redirectToRoute('login', array('last_username' => $user->getUsername()));
+			}
+		}
+
+		return $this->render('Security/register.html.twig', array(
+			'form'  => $form->createView(),
 		));
 	}
 }
---
+++ src/Form/UserType.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username',
+                TextType::class,
+                array(
+                    'label' => "Identifiant ENT",
+                    'required' => true
+                )
+            )
+            ->add('password',
+                RepeatedType::class,
+                array(
+                    'type' => PasswordType::class,
+                    'label' => "Mot de passe",
+                    'required' => true,
+                    'options' => ['attr' => ['class' => 'password-field']],
+                    'invalid_message' => 'Les  mots de passe doivent correspondre.',
+                    'first_options'  => ['label' => 'Mot de passe'],
+                    'second_options' => ['label' => 'Confirmer le mot de passe'],
+                    'attr' => [ 'class' => "au-input au-input--full" ]
+                )
+
+            )
+            ->add('name',
+                TextType::class,
+                array(
+                    'label' => 'Nom',
+                    'required' => true
+                )
+            )
+            ->add('email',
+                EmailType::class,
+                array(
+                    'label' => 'Adresse e-mail',
+                    'required' => true
+                )
+            )
+            ->add('birthday',
+                DateType::class,
+                array(
+                    'label' => "Date de naissance",
+                    'required' => true,
+                    'widget' => 'single_text',
+                )
+            )
+            ->add('public',
+                CheckboxType::class,
+                array(
+                    'label' => "Rendre mon profil visible dans l'annuaire",
+                    'required' => false
+                )
+            )
+            ->add('profilimage',
+                FileType::class,
+                array(
+                    'label' => "Image de profil",
+                    'required' => false,
+                    'empty_data' => "/img/user.png"
+                )
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
