89da7eac82a264823cefad1218322ac2fad249ad
Nicolas TOURRETTE <nicotou1998@gmail.com>
2020-12-04 15:22:16 +0100

    Update Symfony to 5.2.0

--- src/Repository/AnnaleRepository.php
+++ src/Repository/AnnaleRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Annale;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Annale|null find($id, $lockMode = null, $lockVersion = null)
